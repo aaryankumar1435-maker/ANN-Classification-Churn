df925de0ee4cb09f9d27fc6358606e853fb9398c
Aryan Kumar <aaryankumar1435@gmail.com>
2026-06-22 17:50:52 +0530

    Replace tensorflow with tf-keras for Python 3.14 compatibility

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
+import tf_keras as tf_keras
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf_keras.models.load_model('model.h5')
 
 # Load encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
